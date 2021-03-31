9de042c7afdf59099a06c7164cb957f35cb369b1
MahatmaK <nirajkhatri21@gmail.com>
2021-03-31 00:28:11 -0400

    Completed calculating values for Pypoll. Working on printing results

---
+++ PyPoll/main.py
@@ -0,0 +1,55 @@
+# Import os and csv 
+import os
+import csv
+
+# Variable for total votes
+total_votes = 0
+
+# Array of candidates
+candidates =[]
+
+csvfile = os.path.join('03-Python_Homework_Instructions_PyPoll_Resources_election_data.csv')
+
+with open(csvfile, newline ='') as csv_file:
+    csvreader = csv.reader(csv_file,delimiter = ',')
+    
+    next(csvreader)
+
+    for row in csvreader:
+
+
+        condition = False
+        
+        # If candidate is already in candidates array then add 1 to their vote 
+        for candidate in candidates:
+            if candidate[1] == row[2]:
+                candidate[0] = candidate[0] + 1
+                condition = True
+                break
+        
+            
+        # Add new candidate 
+        if condition == False:
+            new_candidate = [1, row[2]]
+            candidates.append(new_candidate)
+
+
+
+
+candidates.sort(reverse=True)
+print(candidates)
+
+ print("Election Results")
+ print("-------------------------")
+ print(f'Total Votes: {total_votes}')
+ print("-------------------------")
+
+ for candidate in candidates:
+     print(f'{candidate[1]}: {round(candidate[0]/total_votes,3)}% ({candidate[0]})')
+
+
+
+
+                
+            
+
---
+++ PyPoll/test.py
@@ -0,0 +1,3 @@
+list = [0,0]
+list[1]=list[1] + 1
+print(list)
